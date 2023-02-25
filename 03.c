8ca5a870b6c8bf0fb615d6bb4bfa718e6f02fea6
Gihoon <kih0976@gmail.com>
2023-02-26 05:50:10 +0900

    studying 03.c stop 05:30

---
+++ 03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+	int korean;
+	int math;
+	int english;
+
+	korean = 96;
+	math = 96;
+	english = 95;
+
+	// printf 는 값을 출력해주는 함수다.
+	// "(문자열)" 안에 \n 을 입력해주면 '개행'이 된다.
+	printf("국어 : %d\n", korean);
+	printf("수학 : %d\n", math);
+	printf("영어 : %d", english);
+	// 05:30
+}
