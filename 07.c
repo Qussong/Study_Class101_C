f6b0b70bd2199e38e09c5dca5326a6df2d4a8d43
Gihoon <kih0976@gmail.com>
2023-03-06 02:01:24 +0900

    modify 07.c

--- 07.c
+++ 07.c
@@ -1,24 +1,35 @@
 /*
-		<< 조건문 >>
+                << 조건문 >>
 
-	[ if - else ]
-	
-	ex)
-	합격 판정 :
-	점수를 입력 받아 60점 이상이면 "합격" 판정해 보자
+        [ if - else ]
+
+        ex)
+        합격 판정 :
+        점수를 입력 받아 60점 이상이면 "합격" 판정해 보자
 */
 #include <stdio.h>
 
 int main() {
-	int score;
+    int score;
 
-	printf("점수 : ");
-	scanf("%d", &score);
+    printf("점수 : ");
+    scanf("%d", &score);
 
-	/*printf("합격입니다!\n");
-	printf("판정 완료!\n");*/
-	
-	if (score >= 60)
-		printf("합격입니다!\n");  // 입력되는 score 의 점수에 따라 skip 되기도 하고 실행되기도 한다.
-	printf("판정 완료!\n");
+    /*printf("합격입니다!\n");
+    printf("판정 완료!\n");*/
+
+    if (score >= 60)
+        printf("합격입니다!\n");  // 입력되는 score 의 점수에 따라 skip 되기도 하고 실행되기도 한다.
+        /*
+            if 가 이끄는 구문이 한줄이면 블록 처리를 안해도 되지만
+            두줄 이상이 되면 블록처리를 해줘야 한다.
+            블록 = 증괄호로 묶어준 영역
+            ex)
+                if (score >= 60) {
+                    printf("합격입니다!\n");
+                    printf("축하합니다!\n");
+                }
+        */
+    printf("판정 완료!\n");
 }
+// 07:40
