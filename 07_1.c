bed557d5d619d2e48007cbc8fbf3763689db64f9
Gihoon <kih0976@gmail.com>
2023-03-06 02:38:14 +0900

    if-else if-else 예제

---
+++ 07_1.c
@@ -0,0 +1,64 @@
+/*
+	합격/불합격 판정 :
+	점수를 입력 받아 60점 이상이면 "합격" 판정해 보자.
+	"불합격"알 떄도 알려 준다.
+
+	결과_1)
+	점수 : 61
+	합격입니다.
+	판정 완료!
+
+	결과_2)
+	점수 : 55
+	불합격입니다.
+	판정 완료!
+*/
+
+/*#include <stdio.h>
+
+int main() {
+	int score;
+
+	printf("점수 : ");
+	scanf("%d", &score);
+	if (score >= 60)
+		printf("합격입니다.\n");
+	else
+		printf("불합격입니다.\n");
+	printf("판정 완료!\n");
+}*/
+
+/*
+	학점 판정 :
+	점수를 입력 받아 90점 이상이면 A학점을, 80점 이상이면 B학점을, 
+	70점 이상이면 C 학점을, 그 외는 F 학점을 부여한다.
+
+	결과_1)
+	점수 : 90
+	A학점 입니다
+	판정 완료!
+	
+	결과_2)
+	점수 : 59
+	F학점입니다.
+	판정 완료!
+*/
+
+/*#include <stdio.h>
+
+int main() {
+	int score;
+
+	printf("점수 : ");
+	scanf("%d", &score);
+
+	if (score >= 90)
+		printf("A학점입니다.\n");
+	else if (score >= 80)
+		printf("B학점입니다.\n");
+	else if (score >= 70)
+		printf("C학점입니다.\n");
+	else
+		printf("F학점입니다.\n");
+	printf("판정 완료!\n");
+}*/
